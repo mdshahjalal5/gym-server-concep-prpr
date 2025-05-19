c1eb9b3ce564eae7bd41e3eca2c48745f8cfc97d
sj <muhommodshahjalal@gmail.com>
2025-05-19 07:40:52 +0600

    Initial commit

---
+++ src/App/config/db.js
@@ -0,0 +1,38 @@
+import { MongoClient, ServerApiVersion } from "mongodb";
+import dotenv from "dotenv";
+dotenv.config();
+const uri =
+  process.env.MONGODB_URI ||
+  "mongodb+srv://alluser:shahjalal@cluster0.aneroim.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+let db;
+export async function connectToDb() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    db = client.db("gym");
+    console.log(
+      "Alhamdulillah, MongoDB is connected  and the server is running!",
+    );
+  } catch (err) {
+    console.log("❌ Failed to connect to MongoDB:", err);
+  }
+}
+
+export const getDb = () => {
+  if (!db) {
+    throw new Error("❌ DB is not connected, please call connectToDB first");
+  }
+  return db;
+};
